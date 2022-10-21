ad1f61d866beae222d7201f9bb4779e87b8abe89
Omkar Goud <108296226+om1895@users.noreply.github.com>
2022-10-21 18:41:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def homepage():
-    return "<h1>Welcome to MentorKart...!!<h1>"
+    return "<h1>Welcome to MentorKart@om...!!<h1>"
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=8000)
